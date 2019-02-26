45943783bfd086872c06f0b74a1b5b48f2351328
Ragnhild Aalvik <raa009@student.uib.no>
2019-02-26 22:29:17 +0100

    Deleted not useful stuff from workshop

--- scripts/oppgave10.js
+++
@@ -1,9 +0,0 @@
-// Description:
-//   Boten henter sanntidsinformasjon fra Ruter
-//
-// Commands:
-//   hubot neste buss fra skuret
-//   hubot neste tbane fra <skolen din>
-
-// Hint: https://reisapi.ruter.no/help
-module.exports = function (robot) {}
