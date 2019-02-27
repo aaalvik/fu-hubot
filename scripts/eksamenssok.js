ada9134f1ea137c283fcb1e9a65fb931f77711fd
Ragnhild Aalvik <raa009@student.uib.no>
2019-02-27 10:31:00 +0100

    Added exam search

---
+++ scripts/eksamenssok.js
@@ -0,0 +1,45 @@
+const cheerio = require('cheerio');
+
+const URL = 'https://www.uib.no/student/eksamensplan/matnat';
+
+module.exports = robot => {
+  robot.respond(/eksamen (\w+)/, res => {
+    const emne = (res.match[1]).toUpperCase();
+    robot.http(URL).get()((error, response, htmlBody) => {
+      const $ = cheerio.load(htmlBody);
+
+      const fag = $('.item-list .faculty-exam-list li')
+        .map((index, element) => {
+          const [dato, tidspunkt] = $(element)
+            .find('.uib-study-exam-assessment dt:first-of-type + dd')
+            .text()
+            .split(/\s*,\s+/);
+          const sted = $(element)
+            .find('.uib-study-exam-assessment')
+            .text()
+            .replace(/^.*Sted\s*(.*)/gs, '$1')
+            .trim();
+          return {
+            navn: $(element)
+              .find('.exam-list-title')
+              .text()
+              .replace(/.*\/\s*(.*)/, '$1')
+              .trim(),
+            emnekode: $(element)
+              .find('.exam-list-title a')
+              .text(),
+            dato,
+            tidspunkt,
+            sted
+          };
+        })
+        .get();
+      const valgtEmne = fag.find(f => f.emnekode === emne);
+      res.send(
+        `${valgtEmne.navn}: ${valgtEmne.dato} kl ${valgtEmne.tidspunkt} på ${
+          valgtEmne.sted
+        }`
+      );
+    });
+  });
+};
